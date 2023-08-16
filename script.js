37b0f5aaacca3831e192c89da51e37b3fc764f09
M. Jesus Gonzalez <marcelogonzalez1418@hotmail.com>
2023-08-16 17:44:22 -0500

    update script with validations

--- script.js
+++ script.js
@@ -1,33 +1,50 @@
 const fileInput = document.getElementById("fileInput");
-
 const fileInfo = document.getElementById("fileInfo");
-
 const preview = document.getElementById("preview");
 
 fileInput.addEventListener("change", function (event) {
   const selectedFile = event.target.files[0];
 
-  fileInfo.innerHTML = `
+  if (selectedFile) {
+    const validExtensions = ["jpg", "jpeg", "png"];
+    const maxFileSize = 5 * 1024 * 1024; // 5MB in bytes
 
-    Nombre del archivo: ${selectedFile.name}<br>
+    const fileExtension = selectedFile.name.split(".").pop().toLowerCase();
+    const fileType = selectedFile.type.toLowerCase();
 
-    Tipo MIME: ${selectedFile.type}<br>
+    if (
+      validExtensions.includes(fileExtension) &&
+      selectedFile.size <= maxFileSize &&
+      (fileType === "image/jpeg" ||
+        fileType === "image/jpg" ||
+        fileType === "image/png")
+    ) {
+      fileInfo.innerHTML = `
+        Nombre del archivo: ${selectedFile.name}<br>
+        Tipo MIME: ${selectedFile.type}<br>
+        Tamaño: ${selectedFile.size} bytes
+      `;
 
-    Tamaño: ${selectedFile.size} bytes
+      if (fileType.startsWith("image/")) {
+        preview.style.display = "block";
 
-  `;
+        const reader = new FileReader();
 
-  if (selectedFile.type.startsWith("image/")) {
-    preview.style.display = "block";
+        reader.onload = function (e) {
+          preview.src = e.target.result;
+        };
 
-    const reader = new FileReader();
-
-    reader.onload = function (e) {
-      preview.src = e.target.result;
-    };
-
-    reader.readAsDataURL(selectedFile);
+        reader.readAsDataURL(selectedFile);
+      } else {
+        preview.style.display = "none";
+      }
+    } else {
+      fileInfo.innerHTML =
+        "Archivo no válido. Por favor, sube un archivo JPG o PNG de máximo 5MB.";
+      preview.style.display = "none";
+    }
   } else {
+    fileInfo.innerHTML = "No se ha seleccionado ningún archivo.";
     preview.style.display = "none";
   }
 });
